e23973996fc43d2ec0f94d544255b0961ecb5c6a
mikekavouras <mike@wantful.com>
2013-09-17 14:27:25 -0400

    add and update reps

--- app/assets/javascripts/admin/contacts/managers/RepManager.js
+++ app/assets/javascripts/admin/contacts/managers/RepManager.js
@@ -1,17 +1,20 @@
 function RepManager() {
-
-  var manager = this;
   this.reps = new RepsCollection();
   this.reps.fetch();
 }
 
 RepManager.prototype = {
   showModal: function(id, $elem) {
+    var $body = $('body');
+    if ($body.hasClass('has-modal')) return;
+
+    $body.addClass('has-modal');
+
     var rep = id ? this.reps.get(id).toJSON() : null;
     var modal = this.getModalHTML(rep);
     var $modal = $(modal);
-    $('body').append($modal);
 
+    $body.append($modal);
     $modal.show();
 
     var modalHeight = $modal.outerHeight();
@@ -27,54 +30,125 @@ RepManager.prototype = {
     });
 
     $modal.show();
+
+    var manager = this;
+
+    $('#cancel').bind('click', function() {
+      $body.removeClass('has-modal');
+      manager.hideModal();
+    });
+
+    $('#add-rep').bind('click', function() {
+      // validate
+      var data = {
+        location: $('#location').val(),
+        reps: [
+          {
+            name: $('#rep-one-name').val(),
+            email: $('#rep-one-email').val()
+          },
+          {
+            name: $('#rep-two-name').val(),
+            email: $('#rep-two-email').val()
+          }
+        ]
+      };
+
+      manager.updateRepLocation(id, data);
+      /*
+      var success = function() {
+        $elem.replaceWith($(manager.repTemplate(data)));
+        $body.removeClass('has-modal');
+        manager.hideModal();
+      };
+
+      if (id) manager.updateRepLocation(id, data, success);
+      else manager.addRepLocation(data, success);
+      */
+    });
+  },
+
+  hideModal: function() {
+    $('#rep-modal-container').remove();
   },
 
   addRepLocation: function(data, success) {
     var rep = new RepModel(data);
-    this.repsCollection.add(rep);
+    this.reps.add(rep);
+    if (success) success();
+  },
+
+  updateRepLocation: function(id, data, success) {
+    rep = this.reps.get(id);
+    console.log(rep, data);
+    return;
+    rep.set(data);
     if (success) success();
   },
 
   removeRep: function(id, success) {
-    var model = this.repsCollection.get(id);
-    this.repsCollection.remove(model);
+    var model = this.reps.get(id);
+    this.reps.remove(model);
     if (success) success();
-  }
-
-  hideEditModal: function() {
-
   },
 
   getModalHTML: function(repLocation) {
-
     var html = "";
     html = '<div id="rep-modal-container">';
     html += '<div id="rep-modal">';
 
     html += '<label class="light">location</label>';
-    html += '<input class="light" type="text" value="';
+    html += '<input class="light" type="text" id="location" value="';
     if (repLocation)
       html += repLocation.location;
     html += '"></input><br>';
 
     html += '<label class="light">name</label>';
-    html += '<input class="light" type="text" value="';
+    html += '<input class="light" id="rep-one-name"type="text" value="';
     if (repLocation && repLocation.reps[0])
       html += repLocation.reps[0].name;
     html += '"></input><br>';
 
     html += '<label class="light">email</label><br>';
-    html += '<input class="light" type="text" value="';
+    html += '<input class="light" type="text" id="rep-one-email" value="';
     if (repLocation && repLocation.reps[1])
       html += repLocation.reps[1].name;
     html += '"></input><br>';
 
     html += '<button id="cancel" class="btn" style="width: 48%;">cancel</button>';
-    html += '<button id="add-rep" class="btn" style="width: 48%;margin-left:8px;background-color:#000;">add</button>';
+    if (repLocation)
+      html += '<button id="add-rep" class="btn" style="width: 48%;margin-left:8px;background-color:#000;">update</button>';
+    else
+      html += '<button id="add-rep" class="btn" style="width: 48%;margin-left:8px;background-color:#000;">add</button>';
 
     html += '</div>';
     html += '</div>';
 
+    return html;
+  },
+
+  repTemplate: function(data) {
+    var html = '';
+    html += '<div class="rep">';
+    html += '<p class="location">';
+    if (data.location) {
+      html += data.location;
+    }
+    html += '</p>';
+
+    html += '<p class="bold">';
+    if (data.nameOne) {
+      html += data.nameOne;
+    }
+    html += '</p>';
+
+    html += '<p class="bold">';
+    if (data.nameTwo) {
+      html += data.nameTwo;
+    }
+    html += '</p>';
+
+    html += '</div>';
     return html;
   }
 }
