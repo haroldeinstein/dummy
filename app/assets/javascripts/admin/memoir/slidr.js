384f4d92ad57c9185ed8e9be88e9b49cad2eafb4
mikekavouras <mike@wantful.com>
2013-09-23 18:43:08 -0400

    lets change how we do that

--- app/assets/javascripts/admin/memoir/slidr.js
+++ app/assets/javascripts/admin/memoir/slidr.js
@@ -1,11 +1,12 @@
 function Slidr() {
   this.$container = null;
   this.containerWidth = 0;
+  this.idx = 0;
 }
 
 Slidr.prototype = {
   init: function() {
-
+    this.bindEvents();
   },
 
   resize: function() {
@@ -16,17 +17,10 @@ Slidr.prototype = {
 
   showEmptyPost: function() {
     var $post = $(this.emptyPostHTML());
-    var $uploader = $(".image-uploader").first().clone().removeClass('hide');
-    var $container = $('#post-container');
-    var newWidth = $container.outerWidth() + this.containerWidth;
-    $container.css('width', newWidth);
-    $('#post-container').prepend($post);
+    var $uploader = $('.image-uploader').clone();
+    $uploader.removeClass('hide');
     $post.find('.image').append($uploader);
-    $post.addClass('new').animate({'width': this.containerWidth}, 800);
-
-    $post.find('.captions').bind('click', function(e) {
-      $(this).replaceWith('<textarea class=""></textarea>');
-    });
+    $('body').append($post);
 
     $post.find('input[type="file"]').bind('change', function(e) {
       var input = $(this)[0];
@@ -39,19 +33,58 @@ Slidr.prototype = {
 
       var src = reader.readAsDataURL(input.files[0]);
     });
-
   },
 
   emptyPostHTML: function() {
     var html = '';
+    html += '<div id="modal">';
     html += '<div class="post clearfix">';
     html += '<div class="image">';
     html += '<img src="">';
     html += '</div>';
     html += '<div class="captions empty">';
     html += '<p class="caption"></p>';
-    html += '</div></div>';
+    html += '</div></div></div>';
 
     return html;
+  },
+
+  seekTo: function(index) {
+    $('#post-container').animate({ 'left' : -this.containerWidth * index });
+  },
+
+  nextPost: function() {
+    var test = $('.post').length - 1;
+    this.idx++;
+    if (this.idx > test) {
+      this.idx = test;
+      alert('last post');
+      return;
+    }
+
+    this.seekTo(this.idx);
+  },
+
+  previousPost: function() {
+    this.idx--;
+    if (this.idx < 0) {
+      this.idx = 0;
+      alert('first post');
+      return;
+    }
+
+    this.seekTo(this.idx);
+  },
+
+  bindEvents: function() {
+    var slider = this;
+
+    $('#next').bind('click', function() {
+      slider.nextPost();
+    });
+
+    $('#previous').bind('click', function() {
+      slider.previousPost();
+    });
   }
 };
--- app/assets/javascripts/memoirs_admin.js
+++ app/assets/javascripts/memoirs_admin.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   slider.$container = $('#main-container');
   slider.containerWidth = slider.$container.innerWidth() * 0.9;
   slider.resize();
+  slider.init();
 
   $(window).bind('resize', function() {
     slider.resize();
