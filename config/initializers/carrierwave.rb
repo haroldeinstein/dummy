63cba86059f171843964052a37978eb7e4937c8c
mikekavouras <mike@wantful.com>
2013-09-23 10:37:37 -0400

    remove s3 uploading because its a bitch

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     AWS_CONFIG["app_key"],
-    aws_secret_access_key: AWS_CONFIG["app_secret"]
-  }
-
-  config.fog_directory = AWS_CONFIG["s3_bucket"]
-end
