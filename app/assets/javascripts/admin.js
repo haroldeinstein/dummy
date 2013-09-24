e141168b790089ed9ca966e7004647ffc9db2109
mikekavouras <mike@wantful.com>
2013-09-24 17:04:55 -0400

    save new directors

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -3,26 +3,31 @@
 //= require libs/jquery-ui-min.js
 
 $(document).ready(function() {
-  $('.new-director').bind('click', function() {
+  $('#navbar').on('click', '.new-director', function() {
     var $li = $(this);
     var $input = $('<input type="text"></input>');
     $(this).replaceWith($input);
     $input.focus().bind('blur', function() {
       var $self = $(this);
       var val = $self.val();
-      $self.replaceWith('<li><h2 class="home"><a href="/admin/' + val.toLowerCase().replace(' ', '-') + '">' + val.toLowerCase() + '</a></h2></li>');
+      if ($.trim(val) === "") {
+        $self.replaceWith('<li class="new-director"><div class="add"></div><h3>new director</h3></li>');
+      } else {
+        var $replacement = $('<li><h2 class="home"><a href="/admin/' + val.toLowerCase().replace(' ', '-') + '">' + val.toLowerCase() + '</a></h2></li>');
+        $self.replaceWith($replacement);
 
-      $.ajax({
-        url: '/api/admin/directors',
-        data: {
-          authenticity_token: $('meta').filter('[name="csrf-token"]').attr('content'),
-          director: {name: val}
-        },
-        type: 'POST',
-        success: function(response) {
-          console.log(response);
-        }
-      });
+        $.ajax({
+          url: '/api/admin/directors',
+          data: {
+            authenticity_token: $('meta').filter('[name="csrf-token"]').attr('content'),
+            director: {name: val}
+          },
+          type: 'POST',
+          success: function(response) {
+            $replacement.after('<li class="new-director"><div class="add"></div><h3>new director</h3></li>');
+          }
+        });
+      }
     });
   });
 });
