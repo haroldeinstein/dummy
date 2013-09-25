a18fd26947087be5600b3d5bc61fbfe786983fdd
mikekavouras <mike@wantful.com>
2013-09-25 18:04:35 -0400

    dependant

--- app/models/rep_location.rb
+++ app/models/rep_location.rb
@@ -1,7 +1,7 @@
 class RepLocation < ActiveRecord::Base
   attr_accessible :location
 
-  has_many :reps
+  has_many :reps, dependent: :destroy
 
   def as_json(options={})
     serializable_hash(only: [
