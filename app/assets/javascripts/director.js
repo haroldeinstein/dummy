1414cc21861bea178a73af81750d06df4672b2b2
mikekavouras <mike@wantful.com>
2013-09-25 11:21:12 -0400

    links on directors page

--- app/assets/javascripts/director.js
+++ app/assets/javascripts/director.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('#videos-list').on('click', 'h3', function() {
+  $('#videos-list').on('click', 'a', function(e) {
+    e.preventDefault();
     var id = $(this).attr('data-vimeoid');
     var url = "//player.vimeo.com/video/"+id;
     $('#vimeo-player').attr('src', url);
