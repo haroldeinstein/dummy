bf6318d9cf9c97d83076e6c40dfbe6a2bfccaea7
mikekavouras <mike@wantful.com>
2013-10-29 22:50:48 -0400

    autoplay

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     if ($(window).width() <= 320) return;
     e.preventDefault();
     var id = $(this).attr('data-vimeoid');
-    var url = "//player.vimeo.com/video/"+id;
+    var url = "//player.vimeo.com/video/"+id+"?autoplay=1";
     var iframe = '<iframe id="vimeo-player" src="' + url + '" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     if ($('#vimeo-player').length === 0) {
       $('#video-player').html(iframe);
