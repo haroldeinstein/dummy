7ebdf96ad12a75ab3816a67d650c3b83aefcee5c
mikekavouras <mike@wantful.com>
2013-11-01 10:06:01 -0400

    unique vimeo id

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :director_id, :title, :video_url, :thumbnail_small, :thumbnail_medium, :thumbnail_large, :vimeo_id, :sort_index, :created_at, :updated_at
 
   validate :director_id, presence: true
+  validate :vimeo_id, uniqueness: true
 end
 
 # == Schema Information
