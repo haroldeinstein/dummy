9301b67186a12b192509dfe21152c4623a271a27
mikekavouras <mike@wantful.com>
2013-11-05 14:50:59 -0500

    unless

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authenticate
-    if Rails.env.staging?
+    unless Rails.env.development?
       authenticate_or_request_with_http_basic do |username, password|
         username == "dummyfilms" && password == "harold"
       end
