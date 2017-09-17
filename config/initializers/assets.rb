b1eb2210d5b2cb95816de06d35f0d2e42eb5bd4d
Mike Kavouras <kavourasm@gmail.com>
2017-09-16 23:18:53 -0400

    add assets.rb

---
+++ config/initializers/assets.rb
@@ -0,0 +1,4 @@
+Rails.application.config.assets.precompile += %w(
+welcome.css base.js base.css directors.css login.css admin.css admin.js
+directors_admin.js contact.css memoir.css memoirs.js news.js director.js
+contacts_admin.js memoirs_admin.js news_admin.js )
