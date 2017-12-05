2bae151863cf62b43a1dc06d6db8ee86d0773182
Mike Kavouras <kavourasm@gmail.com>
2017-12-05 15:48:24 -0600

    redirect to root

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,5 +1,9 @@
 class DirectorsController < ApplicationController
   def index
+    unless director && director.active?
+      return redirect_to root_url
+    end
+
     @video_id = director.projects.order("sort_index").first.try(:vimeo_id)
     @video_id = Project.find(params[:vid]).vimeo_id if params[:vid].present?
   end
